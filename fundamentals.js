8b9e93ce1138b33047d0ab155e50f2365c15654c
Guanlan Ji <228298909@qq.com>
2022-09-13 10:33:24 +0100

    Nested objects.

--- fundamentals.js
+++ fundamentals.js
@@ -1,2 +1,17 @@
-const aCar = { owner : "Joe Bloggs", type : 'Toyota Corolla 1.8', registration : '201WD1058' };
-console.log(aCar.owner);
+const aCar = {
+    owner: "Joe Bloggs",
+    type: {
+        make: "Toyota ",
+        model: "Corolla",
+        cc: 1.8
+    },
+    registration : {
+        year: "201",
+        county_code: "WD",
+        number: "1058"
+    }
+
+};
+
+console.log(aCar.owner + ' drives a ' + aCar.type.make);
+console.log("Reg. = " + aCar.registration.year + "-" + aCar.registration.county_code + "-" + aCar.registration.number);
